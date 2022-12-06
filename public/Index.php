55b325ef033cd697ca6d77bde10c1430f266efce
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2022-12-06 17:53:26 -0300

    Initial commit

---
+++ public/Index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login | DOCprot</title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="#"> <!~~ adicionar~~>
+	<link rel="preconnect" href="https://fonts.googleapis.com"/>
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap">
+</head>
+<body>
+	<div id="login">
+		<div class="logo"> 
+			<h1><img src="../assets/img/doc.prot.png" width="500px"></h1>
+		</div>
+	</div>
+
+</body>
+</html>
